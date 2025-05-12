5f4eb21f67d850d889613d694b6baad860c67523
i10veU <briyan0403@gmail.com>
2025-05-12 22:58:58 +0900

    ChangeDirectory

--- main.c
+++ main.c
@@ -25,6 +25,16 @@ int main(void){
         if (fgets(input, maxline, stdin))==NULL break;//한줄 입력
         input[strcspn(input, "\n")] = 0; //strcount stop not : 개행 제거
         if(strcmp(input, "exit")==0) break; //exit나오면 탈출
+
+        //ChangeDIrectory
+        if (strncpy(input, "cd", 3)==0){ //입력 cd 확인, strcpy와 달리 3으로 제한해 overflow 방지
+            char *path = input + 3; //경로 추출
+            if (chdir(path) != 0){ //dir 변경 시도 경로 맞으면 0, 틀리면 -1 : 절대/상대경로 모두 가능
+                perror("Not Valid Path"); //error message
+            }
+            continue;
+        }
+
     }
 
     return 0;
