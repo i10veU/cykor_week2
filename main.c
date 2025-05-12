3894ca9c5e381043832d4b85ffe220dabdd50214
i10veU <briyan0403@gmail.com>
2025-05-12 17:51:56 +0900

    쉘의 current working directory, input gcc makefile

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> //getCurrentWorkingdirectory
+#include <string.h> //strCoMPare if same return 0
+
+#define maxline 1024
 
 int main(void){
+    char input[maxline];
+
+    while(1){
+        char cwd[1024];//cwd 쉘에 표시
+        getcwd(cwd, sizeof(cwd));
+        printf("%s", cwd);
+
+        if (fgets(input, maxline, stdin))==NULL break;//한줄 입력
+        input[strcspn(input, "\n")] = 0; //strcount stop not : 개행 제거
+        if(strcmp(input, "exit")==0) break; //exit나오면 탈출
+    }
 
     return 0;
 }
